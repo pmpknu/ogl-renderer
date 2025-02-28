0fde86f4cebe1493f644827e2e75b9aad2849ea1
pmpknu <dangorlyakov@gmail.com>
2025-02-28 17:52:58 +0300

    feat(math): add vec x,y,z,w fields

--- lib/math/vec.ml
+++ lib/math/vec.ml
@@ -1,5 +1,8 @@
 module type Vec = sig
   type t
+
+  val x : t -> float
+  val y : t -> float
   val zero : t
   val add : t -> t -> t
   val sub : t -> t -> t
@@ -12,6 +15,10 @@ end
 
 module Vec2 : Vec with type t = float * float = struct
   type t = float * float
+
+  let x (x, _) = x
+  let y (_, y) = y
+
   let zero = (0.0, 0.0)
   let add (x1, y1) (x2, y2) = (x1 +. x2, y1 +. y2)
   let sub (x1, y1) (x2, y2) = (x1 -. x2, y1 -. y2)
@@ -22,8 +29,18 @@ module Vec2 : Vec with type t = float * float = struct
   let normalize v = let len = length v in if len = 0.0 then zero else div v len
 end
 
-module Vec3 : Vec with type t = float * float * float = struct
+module type Vec3 = sig
+  include Vec
+  val z : t -> float
+end
+
+module Vec3 : Vec3 with type t = float * float * float = struct
   type t = float * float * float
+
+  let x (x, _, _) = x
+  let y (_, y, _) = y
+  let z (_, _, z) = z
+
   let zero = (0.0, 0.0, 0.0)
   let add (x1, y1, z1) (x2, y2, z2) = (x1 +. x2, y1 +. y2, z1 +. z2)
   let sub (x1, y1, z1) (x2, y2, z2) = (x1 -. x2, y1 -. y2, z1 -. z2)
@@ -34,8 +51,19 @@ module Vec3 : Vec with type t = float * float * float = struct
   let normalize v = let len = length v in if len = 0.0 then zero else div v len
 end
 
-module Vec4 : Vec with type t = float * float * float * float = struct
+module type Vec4 = sig
+  include Vec3
+  val w : t -> float
+end
+
+module Vec4 : Vec4 with type t = float * float * float * float = struct
   type t = float * float * float * float
+
+  let x (x, _, _, _) = x
+  let y (_, y, _, _) = y
+  let z (_, _, z, _) = z
+  let w (_, _, _, w) = w
+
   let zero = (0.0, 0.0, 0.0, 0.0)
   let add (x1, y1, z1, w1) (x2, y2, z2, w2) = (x1 +. x2, y1 +. y2, z1 +. z2, w1 +. w2)
   let sub (x1, y1, z1, w1) (x2, y2, z2, w2) = (x1 -. x2, y1 -. y2, z1 -. z2, w1 -. w2)
